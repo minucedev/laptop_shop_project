03e2f80d548e8ce8e03dc9e2c903124b5e952169
minucedev <ngnmihpuc.1502@gmail.com>
2025-09-09 09:13:21 +0700

    configure security

--- laptop_shop/src/main/java/com/example/laptop_shop/HelloController.java
+++ laptop_shop/src/main/java/com/example/laptop_shop/HelloController.java
@@ -7,7 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "zxcvzxcvHello SpringBoot";
+        return "main";
+    }
+    @GetMapping("/user")
+    public String userPage() {
+        return "This is user page";
+    }
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "This is admin page";
     }
 
 }
